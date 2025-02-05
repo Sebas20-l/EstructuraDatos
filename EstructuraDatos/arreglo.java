0dd72debd4002e4a5428d22df8600793f339e638
Marco Nevarez Reyes <sebastiannevarezr@gmail.com>
2025-02-05 10:08:44 -0600

    programas

---
+++ EstructuraDatos/arreglo.java
@@ -0,0 +1,13 @@
+public class arreglo {
+    public static void main(String[] args) {
+       
+        String[] names = {"Argel", "Avitia", "Carlo", "Mario", "Camila","Cristhian","Lesly","Cesar","Oscar","Rebceca","Justin","Jorge","Rodrigo"}; // Declaración de un arreglo de cadenas
+        
+        
+        for (String nombres : names) {
+            System.out.println(nombres); //Recorrer el arreglo con un for-each
+        }
+        
+    
+    }
+}
---
+++ EstructuraDatos/main.java
@@ -0,0 +1,8 @@
+public class main{
+    //public static void main(String[] args) {
+        //ppt.main(args);
+    //}
+    //public static void main(String[] args){
+        //arreglo.main(args);
+    //}
+}
