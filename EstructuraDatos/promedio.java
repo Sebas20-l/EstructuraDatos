0dfb78c30ca74b57a11f20f2860700694578c29c
Marco Nevarez Reyes <sebastiannevarezr@gmail.com>
2025-02-06 10:08:04 -0600

    segundo

--- EstructuraDatos/promedio.java
+++ EstructuraDatos/promedio.java
@@ -1,4 +1,35 @@
+//autor: Sebastian Nevarez Romano
+//Matricula: 14970
+// 05/02/2025
+//suma de promedios
 
+public class Promedio{
+    public static void main(String[] args) {
+
+    //declarando el arreglo double
+    double[] numeros= {85,90,72};
+
+    //hacer suma
+    double suma= 0.0;
+
+    //uso de cilco for 
+    for (int i = 0; i < numeros.length; i++) {
+        suma+= numeros[i];
+        System.out.println("La calificacion es "+(i+1)+"."+ numeros[i]);
+    }
+
+    //hacer la division
+    double promedio=suma/numeros.length;
+
+    //imprimiendo los resultados
+    System.out.println("suma total:" + suma);
+    System.out.println("promedio:" + promedio);
+
+
+    
+
+    }
+}
 
 
        
