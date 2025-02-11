7109278c4f11765d38111f06f1592f771e40701a
Marco Nevarez Reyes <sebastiannevarezr@gmail.com>
2025-02-11 10:38:05 -0600

    calificaciones StudentGrade

--- EstructuraDatos/main.java
+++ EstructuraDatos/main.java
@@ -1,5 +1,8 @@
+
+//import java.util.function.ObjIntConsumer;
+
 public class main{
-    //public static void main(String[] args) {
+    public static void main(String[] args) {
         //ppt.main(args);
     //}
     //public static void main(String[] args){
@@ -13,5 +16,11 @@ public class main{
         //System.out.println(fullname);
 
     //}
+
+    int result=ObjStudentGrade.calculateFinalGrade(80,75,90,85);
     
+        System.out.println("la calificacion del smestre es"+ result);
 }
+
+}
+
