de5c884134603da2d5eea864f250355ee7df0125
Marco Nevarez Reyes <sebastiannevarezr@gmail.com>
2025-05-19 21:53:20 -0600

    creo que es todo

--- EstructuraDatos/tercer parcial/Main3.java
+++ EstructuraDatos/tercer parcial/Main3.java
@@ -21,7 +21,7 @@ public class Main3 {
         }
      }*/
     
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         EjemploCola miCola = new EjemploCola();
 
         miCola.agregarCliente("Cliente 1");
@@ -41,6 +41,46 @@ public class Main3 {
 /*public static void main(String[] args) {
     
 }*/
+/*public static void main(String[] args) {
+        ListaCircular lista = new ListaCircular();
+        lista.agregar(1);
+        lista.agregar(2);
+        lista.agregar(3);
+        lista.agregar(4);
+
+        lista.mostrar();
+    }
 }
+public static void main(String[] args) {
+        ListaDoblementeEnlazada lista = new ListaDoblementeEnlazada();
+        lista.agregarAlFinal(10);
+        lista.agregarAlFinal(20);
+        lista.agregarAlFinal(30);
+        lista.agregarAlInicio(5);
+
+        lista.mostrarAdelante();   // Salida: 5 10 20 30
+        lista.mostrarAtras();      // Salida: 30 20 10 5
+
+        lista.eliminar(20);        // Elimina el nodo con valor 20
+        lista.mostrarAdelante();   // Salida: 5 10 30
+    }
+}
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+
+        System.out.println("List:");
+        list.display();
+
+        System.out.println("Is 20 in the list? " + list.search(20));
+
+        list.delete(20);
+
+        System.out.println("After deleting 20:");
+        list.display();
+    }
 }
 
